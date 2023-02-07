8550a3919848b98bb75c95cdf54f025d3c9c797b
ratiry <ratiry@yandex.ru>
2023-02-07 17:22:04 +0300

    made temp handling of victory for cross

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Game from './components/Content/Game/Game';
 import Welcome from './components/Content/Welcome/Welcome';
-import { useReducer, useState } from 'react';
+import { useReducer, useState, useEffect } from 'react';
 import Results from './components/Content/Results/Results';
 import NotFound from './components/Content/NotFound/NotFound';
-
+import CheckForVictory from './Helpers/CheckForVictory';
+let setResul=(tempResult)=>({
+  player:tempResult.player,
+  winning_consequence:tempResult.winning_consequence,
+  direction:tempResult.direction
+})
 function App() {
+  let [IsMoveOfZero,setIsMoveOfZero] = useState(false);
   let [cells,upDateCells]=useState([
     {whose:null,id:0},{whose:null,id:1},{whose:null,id:2},
     {whose:null,id:3},{whose:null,id:4},{whose:null,id:5},
@@ -23,8 +29,26 @@ function App() {
     winning_consequence:null,
     direction:null
   })
-  window.cells=cells;
-  window.occupiedCells=occupiedCells;
+  window.store={
+    cells:cells,
+    occupiedCells:occupiedCells,
+    result:result,
+  }
+  useEffect(()=>{
+    
+  },[])
+  useEffect(()=>{
+    if(occupiedCells.cross.length>=3){
+      let tempResult=CheckForVictory(cells,occupiedCells,3);
+      console.log(tempResult);
+      if(tempResult.player!=null){
+        upDateResult(setResul(tempResult));
+      }
+    }else{
+      setIsMoveOfZero(true);
+    }
+  },[occupiedCells.cross])
+
   return (
     <BrowserRouter>
         <div className={classes.App}>
