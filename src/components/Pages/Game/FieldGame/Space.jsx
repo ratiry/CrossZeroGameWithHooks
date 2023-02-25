9c9d4dad09f6551213fa99c0278771390aaa7836
ratiry <ratiry@yandex.ru>
2023-02-25 14:57:08 +0300

    added meme faces for zero in case of result

--- src/components/Pages/Game/FieldGame/Space.jsx
+++ src/components/Pages/Game/FieldGame/Space.jsx
@@ -12,7 +12,11 @@ let Space=(props)=>{
       props.markSpaceWithCross(props.id,props.upDateCells,props.updateOccupiedCells);
     }
   }
-    
+  const MemeFaces=[
+    <img src='https://previews.123rf.com/images/profshop/profshop1611/profshop161100014/67808755-vector-lol-happy-guy-meme-face-for-any-design-isolated-eps-10.jpg'/>,//defeat
+    <img src='https://w7.pngwing.com/pngs/456/732/png-transparent-sad-character-illustration-internet-meme-sadness-face-crying-jordan-sad-face-meme-miscellaneous-comics-white.png'/>,//victory
+    <img src='https://wallpapers.com/images/hd/poker-face-meme-p3u88a2k153h9x9s.jpg'/>
+  ]
   let Cases=[
     <img src='https://icons.veryicon.com/png/o/miscellaneous/medium-thin-linear-icon/cross-23.png'/>,
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>,
@@ -23,6 +27,7 @@ let ExtraCases=[
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm177.6 62.1C192.8 334.5 218.8 352 256 352s63.2-17.5 78.4-33.9c9-9.7 24.2-10.4 33.9-1.4s10.4 24.2 1.4 33.9c-22 23.8-60 49.4-113.6 49.4s-91.7-25.5-113.6-49.4c-9-9.7-8.4-24.9 1.4-33.9s24.9-8.4 33.9 1.4zM144.4 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm192-32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>,
   null
 ]
+
 let lineCase=0;
 
 let lineCases=[
@@ -52,9 +57,25 @@ if(props.result.player=='zero' || props.result.player=='cross'){
   }else if(props.whose=='cross'){
     Case=0;
   }
+  let memeFaceCase=null;
+  let shouldUseMemes=false;
+  if(props.shouldChangeSymbols & props.result.player!=null & props.whose=='zero'){
+    shouldUseMemes=true;
+    switch(props.result.player){
+      case 'zero':
+        memeFaceCase=0
+        break;
+      case 'cross':
+        memeFaceCase=1
+        break;
+      case 'tie':
+        memeFaceCase=2
+        break;
+    }
+  }
   return(
     <div className={classes.Space}  onClick={onCkickFunction} >
-      {props.shouldChangeSymbols ? ExtraCases[Case] :Cases[Case]}
+      {shouldUseMemes ? MemeFaces[memeFaceCase]:props.shouldChangeSymbols ? ExtraCases[Case] :Cases[Case]}
       {lineCases[lineCase]}
     </div>
   )
